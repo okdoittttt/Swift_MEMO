c3b08efb3a5c70734a1531254cd4f225119590c0
okdoittttt <sonokmoo@gmail.com>
2023-04-09 20:36:35 +0900

    Step2

---
+++ SwiftUIMemo/Memory/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by 손옥무 on 2023/04/09.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate : Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
---
+++ SwiftUIMemo/Memory/MemoStore.swift
@@ -0,0 +1,43 @@
+//
+//  MemoStore.swift
+//  SwiftUIMemo
+//
+//  Created by 손옥무 on 2023/04/09.
+//
+
+import Foundation
+
+// 메모목록을 저장하는 클래스
+class MemoStore: ObservableObject {
+    @Published var list: [Memo]
+    
+    init() {
+        list = [
+            Memo(content: "Hello", insertDate: Date.now),
+            Memo(content: "Awesome", insertDate: .now.addingTimeInterval(3600 * -24)),
+            Memo(content: "SwiftUI", insertDate: .now.addingTimeInterval(3600 * -48))
+        ]
+    }
+    
+    func insert(memo: String) {
+        list.insert(Memo(content: memo), at: 0)
+    }
+    
+    func update(memo: Memo?, content: String) {
+        guard let memo = memo else {
+            return
+        }
+        
+        memo.content = content
+    }
+    
+    func delete(memo: Memo) {
+        list.removeAll { $0.id == memo.id }
+    }
+    
+    func delete(set: IndexSet) {
+        for index in set {
+            list.remove(at: index)
+        }
+    }
+}
