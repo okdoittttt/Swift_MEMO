bc32df00f974d7e094433b65b8f1d229d5cb0e09
okdoittttt <sonokmoo@gmail.com>
2023-04-09 20:26:44 +0900

    Step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 손옥무 on 2023/04/09.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
