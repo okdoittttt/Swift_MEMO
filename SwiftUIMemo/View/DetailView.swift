6708822922793a7159571024313e3a042636d85c
okdoittttt <sonokmoo@gmail.com>
2023-04-09 22:27:33 +0900

    swift memo

--- SwiftUIMemo/View/DetailView.swift
+++ SwiftUIMemo/View/DetailView.swift
@@ -12,6 +12,8 @@ struct DetailView: View {
     
     @EnvironmentObject var store: MemoStore
     
+    @State private var showComposer = false
+    
     var body: some View {
         VStack{
             ScrollView {
@@ -32,12 +34,27 @@ struct DetailView: View {
         }
         .navigationTitle("View Memo")
         .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
+        .sheet(isPresented: $showComposer) {
+            ComposeView(memo: memo)
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(memo: Memo(content: "Hello"))
-            .environmentObject(MemoStore())
+        NavigationView {
+            DetailView(memo: Memo(content: "Hello"))
+                .environmentObject(MemoStore())
+        }
+
     }
 }
